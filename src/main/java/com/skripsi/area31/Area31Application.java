ae1c28710787f74145006b5f5299a15b5444c2a9
MHabib <mhabibofficial@yahoo.com>
2020-02-18 17:22:30 +0700

    #1 init project

---
+++ src/main/java/com/skripsi/area31/Area31Application.java
@@ -0,0 +1,12 @@
+package com.skripsi.area31;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class Area31Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Area31Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/skripsi/area31/Area31ApplicationTests.java
@@ -0,0 +1,11 @@
+package com.skripsi.area31;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class Area31ApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
